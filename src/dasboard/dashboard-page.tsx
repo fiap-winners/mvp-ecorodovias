2746ab327f2083d51a10b8d93a50374a909f597f
Leo Cristofani <leo.cristofani@gmail.com>
2019-11-14 21:38:03 -0300

    initial dashboard page

--- src/dasboard/dashboard-page.tsx
+++ src/dasboard/dashboard-page.tsx
@@ -1,5 +1,61 @@
 import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <Container style={{ marginTop: 30 }}>
+      <Row>
+        <Col xs={6} md={3} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={6} md={3} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={6} md={3} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={6} md={3} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={6} style={{ marginBottom: 30 }}>
+          <Card>
+            <Card.Header>Title goes here</Card.Header>
+            <Card.Body>
+              <Card.Text>Text</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
